0abe7b1000d805e81f4a22fb6285a1f7b9565619
Kun17 <31641386+Kun17@users.noreply.github.com>
2024-01-21 22:07:33 -0800

    Create Traverse_binary_tree_preorder.java

---
+++ LeetCode/Traverse_binary_tree_preorder.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    // public List<Integer> preorderTraversal(TreeNode root) {
+    //     List<Integer> ans = new LinkedList<Integer>();
+    //     if (root == null) return ans;
+    //     ans.add(root.val);
+    //     ans.addAll(preorderTraversal(root.left));
+    //     ans.addAll(preorderTraversal(root.right));
+    //     return ans;
+    // }
+    
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> ans = new LinkedList<Integer>();
+        if (root == null) return ans;
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        stack.push(root);
+        while (!stack.empty()) {
+            TreeNode cur = stack.pop();
+            while (cur != null) {
+                stack.push(cur);
+                ans.add(cur.val);
+                cur = cur.left;
+            }
+            if (stack.empty()) break;
+            cur = stack.pop();
+            stack.push(cur.right);
+        }
+        return ans;
+    }
+}
